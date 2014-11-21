81d342651c151c2b433d67598c9f38eaf8a9b58b
Lars Vogel <Lars.Vogel@vogella.com>
2014-11-21 19:31:39 +0100

    ContexualActionBar adjusted

--- com.vogella.android.listview.contextualactionbar/src/de/vogella/android/listactivity/MyListActivityActionbar.java
+++ com.vogella.android.listview.contextualactionbar/src/de/vogella/android/listactivity/MyListActivityActionbar.java
@@ -11,17 +11,23 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.Toast;
 
-public class MyListActivityActionbar extends ListActivity {
+public class MyListActivityActionbar extends ListActivity 
+		implements ActionMode.Callback {
 
 	protected Object mActionMode;
 	public int selectedItem = -1;
 
+	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
-		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-				"Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-				"Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-				"OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2" };
+		String[] values = new String[] { "Android", 
+				"iPhone", "WindowsMobile", 
+				"Blackberry", "WebOS", "Ubuntu",
+				"Windows7", "Max OS X", "Linux", "OS/2", 
+				"Ubuntu", "Windows7", "Max OS X", 
+				"Linux", "OS/2", "Ubuntu",
+				"Windows7", "Max OS X", 
+				"Linux", "OS/2" };
 
 		MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, values);
 		setListAdapter(adapter);
@@ -29,8 +35,7 @@ public class MyListActivityActionbar extends ListActivity {
 		getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
 
 			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view,
-					int position, long id) {
+			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
 				if (mActionMode != null) {
 					return false;
@@ -38,55 +43,54 @@ public class MyListActivityActionbar extends ListActivity {
 				selectedItem = position;
 
 				// Start the CAB using the ActionMode.Callback defined above
-				MyListActivityActionbar.this
-						.startActionMode(mActionModeCallback);
+				MyListActivityActionbar.this.startActionMode(MyListActivityActionbar.this);
 				view.setSelected(true);
 				return true;
 			}
 		});
 	}
 
-	private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
-
-		// Called when the action mode is created; startActionMode() was called
-		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-			// Inflate a menu resource providing context menu items
-			MenuInflater inflater = mode.getMenuInflater();
-			// Assumes that you have "contexual.xml" menu resources
-			inflater.inflate(R.menu.rowselection, menu);
-			return true;
-		}
-
-		// Called each time the action mode is shown. Always called after
-		// onCreateActionMode, but
-		// may be called multiple times if the mode is invalidated.
-		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-			return false; // Return false if nothing is done
-		}
-
-		// Called when the user selects a contextual menu item
-		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-			switch (item.getItemId()) {
-			case R.id.menuitem1_show:
-				show();
-				// Action picked, so close the CAB
-				mode.finish();
-				return true;
-			default:
-				return false;
-			}
-		}
-
-		// Called when the user exits the action mode
-		public void onDestroyActionMode(ActionMode mode) {
-			mActionMode = null;
-			selectedItem = -1;
-		}
-	};
-
 	private void show() {
-		Toast.makeText(MyListActivityActionbar.this,
-				String.valueOf(selectedItem), Toast.LENGTH_LONG).show();
+		Toast.makeText(MyListActivityActionbar.this, String.valueOf(selectedItem), Toast.LENGTH_LONG).show();
+	}
+
+	// Called when the action mode is created; startActionMode() was called
+	@Override
+	public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+		// Inflate a menu resource providing context menu items
+		MenuInflater inflater = mode.getMenuInflater();
+		// Assumes that you have "contexual.xml" menu resources
+		inflater.inflate(R.menu.rowselection, menu);
+		return true;
+	}
+
+	// Called each time the action mode is shown. Always called after
+	// onCreateActionMode, but
+	// may be called multiple times if the mode is invalidated.
+	@Override
+	public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+		return false; // Return false if nothing is done
+	}
+
+	// Called when the user selects a contextual menu item
+	@Override
+	public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menuitem1_show:
+			show();
+			// Action picked, so close the CAB
+			mode.finish();
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	// Called when the user exits the action mode
+	@Override
+	public void onDestroyActionMode(ActionMode mode) {
+		mActionMode = null;
+		selectedItem = -1;
 	}
 
 }
