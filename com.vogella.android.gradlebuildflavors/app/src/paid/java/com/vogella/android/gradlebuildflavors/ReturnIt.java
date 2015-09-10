28402e3632ad2672a144dd464dc0d0f8a3aff19f
Lars Vogel <Lars.Vogel@vogella.com>
2015-09-10 03:07:11 +0200

    Build flavors with different classes per flavor

---
+++ com.vogella.android.gradlebuildflavors/app/src/paid/java/com/vogella/android/gradlebuildflavors/ReturnIt.java
@@ -0,0 +1,10 @@
+package com.vogella.android.gradlebuildflavors;
+
+/**
+ * Created by vogella on 10.09.15.
+ */
+public class ReturnIt {
+    public static boolean get(){
+        return false;
+    }
+}
