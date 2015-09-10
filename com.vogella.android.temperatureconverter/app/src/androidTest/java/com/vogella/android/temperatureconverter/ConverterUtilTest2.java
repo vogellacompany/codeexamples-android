21322c751d7d04f49498e9b7a79960ccc13f5a82
Lars Vogel <Lars.Vogel@vogella.com>
2015-09-10 02:39:30 +0200

    Usage of Hamcrest

--- com.vogella.android.temperatureconverter/app/src/androidTest/java/com/vogella/android/temperatureconverter/ConverterUtilTest2.java
+++ com.vogella.android.temperatureconverter/app/src/androidTest/java/com/vogella/android/temperatureconverter/ConverterUtilTest2.java
@@ -1,17 +1,17 @@
 package com.vogella.android.temperatureconverter;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.*;
-
-import java.util.Arrays;
-import java.util.List;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertTrue;
-import static org.hamcrest.Matchers.*;
 
-public class ConverterUtilTest {
+@RunWith(AndroidJUnit4.class)
+public class ConverterUtilTest2 {
 
 
     @Test
@@ -21,26 +21,6 @@ public class ConverterUtilTest {
         float expected = 212;
         // use this method because float is not precise
         assertEquals("Conversion from celsius to fahrenheit failed", expected, actual, 0.001);
-    }
-
-    @Test
-    public void testConvertFahrenheitToCelsiusHamcrest() {
-        float actual = ConverterUtil.convertCelsiusToFahrenheit(100);
-        // expected value is 212
-        float expected = 212;
-
-        // Hamcrest
-        assertThat("Conversion from celsius to fahrenheit failed", expected, is(actual));
-    }
-
-    // Hamcrest
-    @Test
-    public void checkListContent() {
-        List<Integer> list = Arrays.asList(5, 2, 4);
-        assertThat(list, hasSize(3));
-        assertThat(list, containsInAnyOrder(5, 2, 4));
-        assertThat(list, containsInAnyOrder(2, 4, 5));
-        assertThat(list, everyItem(greaterThan(1)));
     }
 
     @Test
