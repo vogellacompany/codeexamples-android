3db7f48d8381a78b4130b3908c244099df07b33f
Lars Vogel <Lars.Vogel@vogella.com>
2017-02-21 21:24:08 +0100

    Twitter search app, Touch view updated

--- com.vogella.android.touch.single/app/src/main/java/com/vogella/android/touch/single/SingleTouchActivity.java
+++ com.vogella.android.touch.single/app/src/main/java/com/vogella/android/touch/single/SingleTouchActivity.java
@@ -1,15 +1,23 @@
 package com.vogella.android.touch.single;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
 
 public class SingleTouchActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(new MyViewWithTransparentArea(this));
+		getActionBar().setDisplayShowHomeEnabled(false);
+		getActionBar().s
+		setContentView(new SingleTouchEventView(this, null));
+		int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+		uiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+		uiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+		uiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
+		uiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+		getWindow().getDecorView().setSystemUiVisibility(uiOptions);
 	}
 
 }
