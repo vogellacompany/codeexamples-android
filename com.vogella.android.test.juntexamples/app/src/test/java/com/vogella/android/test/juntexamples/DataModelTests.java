952e80340b6f92752dae540e3f0643fe5d955655
Lars Vogel <Lars.Vogel@vogella.com>
2017-04-11 11:56:02 +0200

    Adds DataModelTests to junit examples

--- com.vogella.android.test.juntexamples/app/src/test/java/com/vogella/android/test/juntexamples/DataModelTests.java
+++ com.vogella.android.test.juntexamples/app/src/test/java/com/vogella/android/test/juntexamples/DataModelTests.java
@@ -1,14 +1,18 @@
 package com.vogella.android.test.juntexamples;
 
+import com.vogella.android.test.juntexamples.model.Movie;
+import com.vogella.android.test.juntexamples.model.Race;
+import com.vogella.android.test.juntexamples.model.Ring;
 import com.vogella.android.test.juntexamples.model.TolkienCharacter;
 
-import org.junit.Assume;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static com.vogella.android.test.juntexamples.model.Race.HOBBIT;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -17,22 +21,21 @@ import static org.junit.Assert.assertTrue;
 
 public class DataModelTests {
 
+    private DataService dataService;
+
     @Test
     public void validateTolkeinCharactorsInitializationWorks() {
         TolkienCharacter frodo = new TolkienCharacter("Frodo", 33, HOBBIT);
-        //age is 33
-        //name is "Frodo"
-        //name is not "Frodon"
+        assertEquals(frodo.age, 33);
+        assertEquals(frodo.getName(), "Frodo");
+        assertNotEquals(frodo.getName(), "Frodon");
     }
 
     @Test
     public void checkEquals(){
         Object jake = new TolkienCharacter("Jake", 43, HOBBIT);
         Object sameJake = jake;
-        Object jakeClone = new TolkienCharacter("Jake",12, HOBBIT);
-        // check that:
-        // jack is equal to sameJake
-        // jack is equal to jakeClone
+        Object jakeClone = new TolkienCharacter("Jake",43, HOBBIT);
         assertEquals(jake, jakeClone);
     }
 
@@ -44,7 +47,7 @@ public class DataModelTests {
         TolkienCharacter tolkienCharacter = dataService.getFellowship().get(0);
         // checkthat tolkienCharacter.getClass is not a movie class
 
-        assertTrue(false);
+        assertTrue(!tolkienCharacter.getClass().equals(Movie.class) );
     }
 
     @Test
@@ -53,16 +56,22 @@ public class DataModelTests {
         List<TolkienCharacter> fellowship = dataService.getFellowship();
 
         // ensure that Frodo and Gandalf are part of the fellowship
-        assertTrue(false);
+        assertTrue(fellowship.contains(dataService.frodo)&& fellowship.contains(dataService.gandalf));
     }
 
     @Test
     public void testThatOneRingBearerIsPartOfTheFellowship() {
         DataService dataService = new DataService();
         List<TolkienCharacter> fellowship = dataService.getFellowship();
-
-        // ensure that Frodo and Gandalf are part of the fellowship
-        assertTrue(false);
+        Map<Ring, TolkienCharacter> ringBearers = dataService.getRingBearers();
+        boolean oneRingBearer = false;
+        for (TolkienCharacter character : ringBearers.values()) {
+            if (fellowship.contains(character)) {
+                oneRingBearer = true;
+            }
+        }
+        // test that at least one ring bearer is part of the fellowship
+        assertTrue(oneRingBearer);
     }
 
     @Test
@@ -72,22 +81,33 @@ public class DataModelTests {
 
         // ensure that the order of the fellowship is:
         //frodo, sam, merry,pippin, gandalf,legolas,gimli,aragorn,boromir
-        assertTrue(false);
+
+        assertEquals(fellowship.get(0),dataService.frodo);
+        assertEquals(fellowship.get(1),dataService.sam);
+        assertEquals(fellowship.get(2),dataService.merry);
+        assertEquals(fellowship.get(3),dataService.pippin);
+        assertEquals(fellowship.get(4),dataService.gandalf);
+        assertEquals(fellowship.get(5),dataService.legolas);
+        assertEquals(fellowship.get(6),dataService.gimli);
+        assertEquals(fellowship.get(7),dataService.aragorn);
+        assertEquals(fellowship.get(8),dataService.boromir);
     }
 
     @Test
     public void ensureAge() {
         DataService dataService = new DataService();
         List<TolkienCharacter> fellowship = dataService.getFellowship();
+        for (TolkienCharacter character : fellowship) {
+            if (character.getRace().equals(Race.HOBBIT) || character.getRace().equals(Race.MAN) ) {
+                assertTrue(character.age < 100);
+            } else {
+                assertTrue(character.age > 100);
 
-        // ensure that all hobbits and men are younger than 100 years
-
-
-        // also ensure that the elfs, dwars the maia are all older than 100 years
-        assertTrue(false);
+            }
+        }
     }
 
-    @Test
+    @Test (expected = IndexOutOfBoundsException.class)
     public void ensureThatFellowsStayASmallGroup() {
         DataService dataService = new DataService();
         List<TolkienCharacter> fellowship = dataService.getFellowship();
