cc265f9346c56d2a04442a45e7faf04e784cfc12
Mayank <bhatnagar.mayank0@gmail.com>
2018-07-03 19:28:05 +0530

    Fixed V13 FragmentStatePagerAdapter which was deprecated

--- com.vogella.android.inlineprogressreporting/src/com/example/android/animationsdemo/ScreenSlideActivity.java
+++ com.vogella.android.inlineprogressreporting/src/com/example/android/animationsdemo/ScreenSlideActivity.java
@@ -20,7 +20,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.PagerAdapter;
